4c796d01f5186b30f5a211ac1126db8c5b8386f8
Aavaahanan <Aavaathebest@gmail.com>
2021-06-29 10:21:32 +0530

    made the basic thing

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import pygame
 
+# init
+pygame.init()
+clock = pygame.time.Clock()
+
+# display
 win_width = 600
 win_height = 480
 
-background_color = (0, 0, 255)
-
 window = pygame.display.set_mode([win_width, win_height])
 
-running = True 
+pygame.display.set_caption("big project")
 
 
+# variables for easy understanding
+fps = 60
+
+# colors
+background_color = (0, 0, 255)
+
+
+# other variables
+running = True
+
+# code
 def update():
+    """things to do every frame"""
+
     window.fill(background_color)
     pygame.display.flip()
 
 
-while running:
+while running: # main game loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     update()
+
+    clock.tick(fps)
